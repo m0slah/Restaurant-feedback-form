9895bdaa80a7be7eaa66a176fa5883e975efdeac
m0slah <muhamemdslahqadir@gmail.com>
2022-11-28 17:06:16 +0300

    set condition for second name input

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,54 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 const Form = () => {
+  const [secondName, setSecondName] = useState();
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (
+      event.target.value.trim().length > 2 &&
+      event.target.value.trim().length <= 12
+    ) {
+      setIsValid(true)
+    }
+  else{
+      setIsValid(false);
+    }
+    setSecondName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (secondName.trim().length === 0) {
+      setIsValid(false);
+    }
+  }
+
   return (
     <div>
       <form>
-        <br />
-        <label>First name</label>
-        <br />
-        <input className="input" type="text" placeholder="first name" />
-        <br />
-        <label>Second Name</label>
-        <br />
-        <input className="input" type="text" placeholder="second name" />
-        <br />
-        <label>Email</label>
-        <br />
-        <input className="input" type="email" placeholder="email" />
-        <label className="input" for="cars">
-          Location You Visited
-        </label>
-        <br />
-        <select value="Duhok" name="cars" id="cars" required>
-          <option value="volvo">Erbil</option>
-          <option value="saab">Sulimanyah</option>
-          <option value="mercedes">Duhok</option>
-          <option value="audi">Karkuk</option>
-        </select>
-        <br />
-        <label>Day Visited</label>
-        <br />
-        <input className="input" type="date" name="datetime" required/>
-        <br />
-        <label>Time Visited</label>
-        <br />
-        <input className="input" type="time" required/>
-        <br />
-        <label>Dine In / Take Out</label>
-        <br />
-        <input type="checkbox" /> <label>Dine In</label>
-        <br />
-        <input type="checkbox" /> <label>Take Out</label>
-        <br />
-        <label>Age</label>
-        <br />
-        <input className="input" type="number" />
-        <br />
-        <label>Any Comments, question or suggesttions?</label>
-        <br />
-              <input className="input comment" type="text" />
-              <button type="submit" className="button">SEND FEDBACK</button>
+        <div>
+          <label>First name</label>
+          <br />
+          <input className="input" type="text" placeholder="first name" />
+        </div>
+
+        <div className={`formControl ${!isValid && "invalid"}`}>
+          <label>Second Name</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            placeholder="second name"
+            onChange={goalInputChangeHandler}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input className="input" type="email" placeholder="email" />
+        </div>
+
+        <div>
+          <label className="input" for="cars">
+            Location You Visited
+          </label>
+          <select value="Duhok" name="cars" id="cars" required>
+            <option value="volvo">Erbil</option>
+            <option value="saab">Sulimanyah</option>
+            <option value="mercedes">Duhok</option>
+            <option value="audi">Karkuk</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Day Visited</label>
+          <br />
+          <input className="input" type="date" name="datetime" required />
+        </div>
+
+        <div>
+          <label>Time Visited</label>
+          <br />
+          <input className="input" type="time" required />
+        </div>
+
+        <div>
+          <label>Dine In / Take Out</label>
+          <br />
+          <input type="checkbox" /> <label>Dine In</label>
+          <br />
+          <input type="checkbox" /> <label>Take Out</label>
+        </div>
+
+        <div>
+          <label>Age</label>
+          <br />
+          <input className="input" type="number" />
+        </div>
+
+        <div>
+          <label>Any Comments, question or suggesttions?</label>
+          <br />
+          <input className="input comment" type="text" />
+        </div>
+
+        <div>
+          <button type="submit" className="button" onSubmit={formSubmitHandler}>
+            SEND FEDBACK
+          </button>
+        </div>
       </form>
     </div>
   );
